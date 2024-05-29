5dca29acafd5ce3f3cab87ded85154b889af8113
jmd <joshua.m.dean@gmail.com>
2024-05-28 21:02:12 -0700

    [widgets] chore: simplify project view

--- src/widgets/projectview.cpp
+++ src/widgets/projectview.cpp
@@ -1,21 +1,110 @@
 #include "projectview.h"
-#include "ui_projectview.h"
-
-#include "projectmodel.h"
 #include "proxymodels.h"
 
-ProjectView::ProjectView(QWidget* parent)
-    : QWidget(parent)
-    , ui(new Ui::ProjectView)
-{
-    ui->setupUi(this);
+#include "nw/kernel/Resources.hpp"
+#include "nw/kernel/Strings.hpp"
+#include "nw/objects/Area.hpp"
+#include "nw/objects/Module.hpp"
 
-    connect(ui->projectTree, &QTreeView::doubleClicked, this, &ProjectView::onDoubleClicked);
+#include <QDir>
+#include <QDirIterator>
+#include <QFileInfo>
+
+// == ProjectItem =============================================================
+// ============================================================================
+
+ProjectItem::ProjectItem(QString name, ProjectItemType type, ProjectItem* parent)
+    : AbstractTreeItem(0, parent)
+    , path_(std::move(name))
+    , type_{type}
+{
+}
+
+ProjectItem::ProjectItem(nw::Area* area, ProjectItemType type, ProjectItem* parent)
+    : AbstractTreeItem(0, parent)
+    , area_(area)
+    , type_{type}
+{
+}
+
+ProjectItem::ProjectItem(nw::Resource res, ProjectItemType type, ProjectItem* parent)
+    : AbstractTreeItem(0, parent)
+    , res_(res)
+    , type_{type}
+{
+    path_ = QString::fromStdString(res_.resref.string());
+}
+
+QVariant ProjectItem::data(int column, int role) const
+{
+    if (column != 0) { return {}; }
+    if (!path_.isEmpty()) {
+        return path_;
+    } else if (area_) {
+        return QString::fromStdString(nw::kernel::strings().get(area_->name));
+    }
+    return {};
+}
+
+// == ProjectModel ============================================================
+// ============================================================================
+
+ProjectModel::ProjectModel(nw::Module* module, QString path, QObject* parent)
+    : AbstractTreeModel(parent)
+    , module_{module}
+    , path_{std::move(path)}
+{
+}
+
+int ProjectModel::columnCount(const QModelIndex& parent) const
+{
+    return 1;
+}
+
+QVariant ProjectModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid()) { return {}; }
+
+    ProjectItem* node = static_cast<ProjectItem*>(index.internalPointer());
+    if (role == Qt::DisplayRole) {
+        return node->data(0);
+    }
+
+    return {};
+}
+
+void ProjectModel::loadRootItems()
+{
+    auto areas = new ProjectItem("Areas", ProjectItemType::category);
+    addRootItem(areas);
+    for (size_t i = 0; i < module_->area_count(); ++i) {
+        auto it = new ProjectItem(module_->get_area(i), ProjectItemType::area, areas);
+        areas->appendChild(it);
+    }
+
+    auto dialogs = new ProjectItem("Dialogs", ProjectItemType::category);
+    addRootItem(dialogs);
+    auto mod = nw::kernel::resman().module_container();
+
+    auto dlg_getter = [dialogs](const nw::Resource& res) {
+        auto it = new ProjectItem(res, ProjectItemType::dialog, dialogs);
+        dialogs->appendChild(it);
+    };
+
+    mod->visit(dlg_getter, {nw::ResourceType::dlg});
+}
+
+// == ProjectView =============================================================
+// ============================================================================
+
+ProjectView::ProjectView(QWidget* parent)
+    : QTreeView(parent)
+{
+    connect(this, &QTreeView::doubleClicked, this, &ProjectView::onDoubleClicked);
 }
 
 ProjectView::~ProjectView()
 {
-    delete ui;
 }
 
 void ProjectView::load(nw::Module* module, QString path)
@@ -30,10 +119,8 @@ void ProjectView::load(nw::Module* module, QString path)
     filter_->setSourceModel(model_);
     filter_->sort(0);
 
-    connect(ui->filter, &QLineEdit::textChanged, filter_, &FuzzyProxyModel::onFilterChanged);
-
-    ui->projectTree->setModel(filter_);
-    ui->projectTree->expandRecursively(filter_->index(0, 0));
+    setModel(filter_);
+    expandRecursively(filter_->index(0, 0));
 }
 
 void ProjectView::onDoubleClicked(const QModelIndex& index)
--- src/widgets/projectview.h
+++ src/widgets/projectview.h
@@ -1,6 +1,11 @@
 #ifndef PROJECTVIEW_H
 #define PROJECTVIEW_H
 
+#include "AbstractTreeModel.hpp"
+
+#include "nw/resources/Resource.hpp"
+
+#include <QTreeView>
 #include <QWidget>
 
 class ProjectItem;
@@ -8,14 +13,61 @@ class ProjectModel;
 class FuzzyProxyModel;
 
 namespace nw {
+struct Area;
 struct Module;
 }
 
-namespace Ui {
-class ProjectView;
-}
+// == ProjectItem =============================================================
+// ============================================================================
 
-class ProjectView : public QWidget {
+enum struct ProjectItemType {
+    category,
+    area,
+    dialog,
+};
+
+class ProjectItem : public AbstractTreeItem {
+public:
+    ProjectItem(nw::Area* area, ProjectItemType type, ProjectItem* parent = nullptr);
+    ProjectItem(QString name, ProjectItemType type, ProjectItem* parent = nullptr);
+    ProjectItem(nw::Resource res, ProjectItemType type, ProjectItem* parent);
+
+    virtual QVariant data(int column, int role = Qt::DisplayRole) const override;
+
+    nw::Area* area_ = nullptr;
+    QString path_;
+    nw::Resource res_;
+    ProjectItemType type_;
+};
+
+// == ProjectModel ============================================================
+// ============================================================================
+
+class ProjectModel : public AbstractTreeModel {
+    Q_OBJECT
+
+public:
+    explicit ProjectModel(nw::Module* module, QString path, QObject* parent = nullptr);
+
+    // Header:
+    // QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    // QAbstractItemModel interface
+    virtual int columnCount(const QModelIndex& parent) const override;
+    virtual QVariant data(const QModelIndex& index, int role) const override;
+
+    // AbstractTreeModel interface
+    virtual void loadRootItems() override;
+
+private:
+    nw::Module* module_ = nullptr;
+    QString path_;
+};
+
+// == ProjectView =============================================================
+// ============================================================================
+
+class ProjectView : public QTreeView {
     Q_OBJECT
 
 public:
@@ -24,18 +76,16 @@ public:
 
     void load(nw::Module* module, QString path);
 
+    nw::Module* module_ = nullptr;
+    QString path_;
+    ProjectModel* model_ = nullptr;
+    FuzzyProxyModel* filter_ = nullptr;
+
 public slots:
     void onDoubleClicked(const QModelIndex& index);
 
 signals:
     void doubleClicked(ProjectItem*);
-
-private:
-    Ui::ProjectView* ui = nullptr;
-    nw::Module* module_ = nullptr;
-    QString path_;
-    ProjectModel* model_ = nullptr;
-    FuzzyProxyModel* filter_ = nullptr;
 };
 
 #endif // PROJECTVIEW_H
